92e98e4b7b51e77d2746eab08f59570e16572837
Joshua Jainga <jjainga@gmail.com>
2020-11-01 18:57:47 -0800

    added router

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,12 +1,10 @@
-const express = require("express");
+const router = require("express").Router();
 const mongoose = require("mongoose");
 
 const db = require("./models");
 
-const app = express();
-
 //retrieving users
-app.get("/user", (req,res) => {
+router.get("/user", (req,res) => {
     db.User.find({})
 }).then(dbUser => {
     res.json(dbUser)
@@ -15,7 +13,7 @@ app.get("/user", (req,res) => {
 )
 
 //creating user
-app.post("/user", ({body}, res) => {
+router.post("/user", ({body}, res) => {
     db.User.create({body})
     .then(dbUser => {
         console.log(dbUser)
@@ -25,7 +23,7 @@ app.post("/user", ({body}, res) => {
 })
 
 //update user
-app.put("/user", (req, res) => {
+router.put("/user", (req, res) => {
     db.User.update(
         {
             _id: mongojs.ObjectId(req.params.id)
